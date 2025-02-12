9b7356f600d1fc3edca35543b8531037e9f9e848
jnywu <johnnywu203@gmail.com>
2025-02-12 17:05:54 +0800

    2.0 init

---
+++ BetterMich/Data/Config.swift
@@ -0,0 +1,10 @@
+//
+//  Config.swift
+//  BetterMich
+//
+//  Created by 吳求元 on 2025/2/7.
+//
+
+public let Version: String = "2.0"
+
+public let MichelinURL: String = "https://guide.michelin.com/tw/zh_TW/selection/taiwan/restaurants"
